5d7daab456ddc73653383131203400d337cd19b9
Harish <harishreddyreddy@gmail.com>
2012-01-12 11:34:04 +1100

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefinerycmsSpike::Application.config.secret_token = 'dbc097832a26a67f213edec77ef3ef503606b194fe29821e7ea99734c84eb5a8e9fc9a76f7b8163230f63b57308ffe49a10d0d162d6a9ac6bd2c04de7649e1d8'
